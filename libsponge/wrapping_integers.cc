66a842d0bf6492b3f033cd4485efdb43704ab880
kevin_sz <250153019@qq.com>
2021-03-03 17:15:35 +0800

    can pass ctest -R wrap; first version.

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,5 +1,7 @@
 #include "wrapping_integers.hh"
 
+#include <iostream>
+
 // Dummy implementation of a 32-bit wrapping integer
 
 // For Lab 2, please replace with a real implementation that passes the
@@ -14,8 +16,8 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint32_t n_32 = (isn.raw_value() + n) % (1ul << 32);
+    return WrappingInt32{n_32};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +31,34 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    WrappingInt32 checkpoint_seqno = wrap(checkpoint, isn);
+    long midway = (1ul << 31);
+    int32_t offset = (n - checkpoint_seqno);
+    if (offset > 0 && offset <= midway)
+        return {checkpoint + offset};
+
+    if (offset > 0 && offset > midway) {
+        uint64_t offset_0 = 2 * midway - offset;
+        if (checkpoint < offset_0)
+            return {checkpoint + offset};
+        else
+            return {checkpoint - offset_0};
+    }
+
+    if (offset == 0)
+        return {checkpoint};
+
+    if (offset < 0 && (offset * -1) > midway) {
+        return {checkpoint + 2 * midway + offset};
+    }
+
+    if (offset < 0 && (offset * -1) <= midway) {
+        uint64_t offset_1 = offset * -1;
+        if (checkpoint < offset_1) {
+            return {checkpoint + 2 * midway + offset};
+        } else {
+            return {checkpoint + offset};
+        }
+    }
+    return {checkpoint};
 }
